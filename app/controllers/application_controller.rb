273d721dd5aedfb89018add7def378245a51735e
Anthony Alberto <alberto.anthony@gmail.com>
2013-03-22 17:24:50 -0700

    Working on the views. Tests ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def after_sign_in_path_for(recruiter)
+    status_index_path
+  end
+  def after_sign_out_path_for(recruiter)
+    new_user_session_path
+  end
 end

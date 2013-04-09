a13a58f9ac09ad219dfb0173d38da2f261d29dad
Anthony Alberto <alberto.anthony@gmail.com>
2013-04-09 12:48:02 -0300

    Changed recruiter to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def after_sign_in_path_for(recruiter)
+  def after_sign_in_path_for(user)
     status_index_path
   end
-  def after_sign_out_path_for(recruiter)
+  def after_sign_out_path_for(user)
     new_user_session_path
   end
 end
